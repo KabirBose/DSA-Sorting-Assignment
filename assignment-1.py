e20e5bd917f0ab5c48b3ef735699a8759ba6dd57
Kabir Bose <kabirbose04@gmail.com>
2024-02-10 00:16:58 -0500

    created add and delete function

--- assignment-1.py
+++ assignment-1.py
@@ -25,7 +25,23 @@ def create_store(data):
 def main():
     product_data = create_store(read_file("product_data.txt"))
 
-    print(product_data["ids"])
+    def add_product(id, name, price, category):
+        product_data["ids"].append(id)
+        product_data["names"].append(name)
+        product_data["prices"].append(price)
+        product_data["categories"].append(category)
+    
+    def delete_product(id):
+        for i in range(len(product_data["ids"])):
+            if (product_data["ids"])[i] == id:
+                (product_data["ids"]).remove((product_data["ids"])[i])
+                (product_data["names"]).remove((product_data["names"])[i])
+                (product_data["prices"]).remove((product_data["prices"])[i])
+                (product_data["categories"]).remove((product_data["categories"])[i])
+            else:
+                print("This ID does not exist!")
+
+    print(product_data["names"])
 
     return 0
 main()
