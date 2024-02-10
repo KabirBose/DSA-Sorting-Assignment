647ddd599854e0e2b0a4d6aa6c7b21910a83284e
Kabir Bose <kabirbose04@gmail.com>
2024-02-10 11:31:11 -0500

    refactored code

--- assignment-1.py
+++ assignment-1.py
@@ -1,55 +1,32 @@
-
 def read_file(my_file):
     data = []
 
     with open(my_file) as file:
         for line in file:
-            data.append(line.rstrip())
+            data.append([line.strip()])
     return data
 
-def create_store(data):
-    store = {
-        "ids": [],
-        "names": [],
-        "prices": [],
-        "categories": []
-    }
-
-    for i in range(len(data)):
-        store["ids"].append(((data[i].split(","))[0]).strip())
-        store["names"].append(((data[i].split(","))[1]).strip())
-        store["prices"].append(((data[i].split(","))[2]).strip())
-        store["categories"].append(((data[i].split(","))[3]).strip())
-    return store
-
 def main():
-    product_data = create_store(read_file("product_data.txt"))
+    product_data = read_file("product_data.txt")
+    products = []
 
-    def add_product(id, name, price, category):
-        product_data["ids"].append(id)
-        product_data["names"].append(name)
-        product_data["prices"].append(price)
-        product_data["categories"].append(category)
-    
-    def delete_product(id):
-        for i in range(len(product_data["ids"])):
-            if (product_data["ids"])[i] == id:
-                (product_data["ids"]).remove((product_data["ids"])[i])
-                (product_data["names"]).remove((product_data["names"])[i])
-                (product_data["prices"]).remove((product_data["prices"])[i])
-                (product_data["categories"]).remove((product_data["categories"])[i])
-            else:
-                print("This ID does not exist!")
+    for i in range(len(product_data)):
+        products.append((product_data[i])[0].split(','))
 
-    def search_product(id, name):
-        for i in range(len(product_data["ids"])):
-            if (product_data["ids"])[i] == id or (product_data["names"])[i] == name:
-                print((product_data["ids"])[i], (product_data["names"])[i], (product_data["prices"])[i], (product_data["categories"])[i])
-            else:
-                print("This product does not exist!")
+    def create(id, name, price, category):
+        products.append([id, name, price, category])
 
-    search_product(0, "Mystery MGSPG")
-    # print(product_data["names"])
+    def search_by_id(id):
+        for i in range(len(products)):
+            if int(products[i][0]) == int(id):
+                print(f"{products[i][0]},{products[i][1]},{products[i][2]},{products[i][3]}")
 
+    def search_by_name(name):
+        for i in range(len(products)):
+            if (products[i][1]).strip() == name:
+                print(products[i][0], products[i][1], products[i][2], products[i][3])
+
+    search_by_id(44574)
+    # print(products)
     return 0
 main()
